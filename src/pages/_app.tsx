0ce9ab16d9a6c25932e90ea24ab87fb36f17afd0
Rafael Almeida <rafaelalmeidatk@gmail.com>
2023-03-21 03:29:35 -0300

    Add Clerk with issue reproduction

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </>
+  );
 }
+
+export default MyApp;
